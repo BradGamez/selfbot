a1909c8d272bcd7ad68a69e1b354b6f822ac568e
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-02-04 15:45:35 -0500

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -6,3 +6,98 @@ var prefix = config.prefix1
 
  
 
+
+
+            bot.on('message', message => {
+                var guild = message.guild;
+                var embed = new Discord.RichEmbed();
+               if(message.author !== bot.user) return;
+                if (message.content.toLowerCase() === prefix + "members") {
+                    message.delete();
+                    embed.setColor('BLUE');
+                    embed.setDescription('Total Members : ' + guild.memberCount + '\nOnline Members : ' + guild.members.filter(m => m.presence.status === "online").size + '\nIdle Members : ' + guild.members.filter(m => m.presence.status === "idle").size + '\nDo Not Disturb Members : ' + guild.members.filter(m => m.presence.status === "dnd").size + '\nOffline Members : ' + guild.members.filter(m => m.presence.status === "offline").size + '\nBot Members : ' + guild.members.filterArray(a => a.user.bot).length + '\nHuman Members : ' + Math.floor(guild.memberCount - guild.members.filterArray(a => a.user.bot).length));
+                    message.channel.send({embed});
+                }})
+
+
+bot.on('message', message => {
+
+    var guild = message.guild;
+
+    var author = message.author;
+
+    var args = message.content.split(' '); var g = " "; for(var i = 1; i < args.length; i++){ g = g+" "+args[i]; }
+
+    if (message.content.startsWith(prefix + 'setplayingstatus')) {
+
+               if(message.author !== bot.user) {
+message.delete()
+            
+
+        bot.user.setGame(g)
+
+        var embed = new Discord.RichEmbed();
+
+        if (message.author.bot) return;
+
+        embed.setColor('BLUE');
+
+        embed.setDescription("Playing status set to**" + g + "**");
+
+      message.channel.send({embed});
+
+    }   
+})
+
+
+                          bot.on('message' , async message => {
+
+                var embed = new Discord.RichEmbed();                                                        
+               if(message.author !== bot.user) return;
+
+                if (message.content.toLowerCase() === prefix + "ping") {
+message.delete()
+   		const m = await message.channel.send("Ping?");
+
+    		var ping = Math.round(m.createdTimestamp - message.createdTimestamp)
+
+                    embed.setColor('BLUE');
+
+                    embed.setDescription("Latency ping " + ping + "ms\nApi ping " + Math.round(bot.ping) + 'ms')
+
+                    m.edit({embed});
+
+                }});           
+ 
+ 
+ 
+ 
+             bot.on('message', message => {
+
+                var guild = message.guild;
+
+                var author = message.author;
+
+                var args = message.content.split(' '); var g = " "; for(var i = 1; i < args.length; i++){ g = g+" "+args[i]; }
+  if(message.author !== bot.user) return;
+                if (message.content.toLowerCase().startsWith(prefix + 'say')) {
+
+                    message.delete();
+
+                    var embed = new Discord.RichEmbed();
+
+                    if (message.author.bot) return;
+
+                    embed.setColor('BLUE');
+
+
+
+                    embed.setDescription(g);
+
+                  message.channel.send({embed});
+
+                }
+
+            });
+
+bot.login(process.env.BOT_TOKEN)
