a417dd739aab085a177c5130bc59824cd6f91297
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-02-05 02:04:35 -0500

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -100,4 +100,16 @@ message.delete()
 
             });
 
+
+bot.on('message', message => {
+                var guild = message.guild;
+                var embed = new Discord.RichEmbed();
+               if(message.author !== bot.user) return;
+                if (message.content.toLowerCase() === prefix + "permissions") {
+                    message.delete();
+                    embed.setColor('BLUE');
+                    embed.setDescription('Total Members : ' + guild.memberCount + '\nOnline Members : ' + guild.members.filter(m => m.presence.status === "online").size + '\nIdle Members : ' + guild.members.filter(m => m.presence.status === "idle").size + '\nDo Not Disturb Members : ' + guild.members.filter(m => m.presence.status === "dnd").size + '\nOffline Members : ' + guild.members.filter(m => m.presence.status === "offline").size + '\nBot Members : ' + guild.members.filterArray(a => a.user.bot).length + '\nHuman Members : ' + Math.floor(guild.memberCount - guild.members.filterArray(a => a.user.bot).length));
+                    message.channel.send({embed});
+                }})
+
 bot.login(process.env.BOT_TOKEN)
